812b2f6b01c58edfd947e6d0fb8d20f063237d42
RehamS21 <reham.mu433@gmail.com>
2023-08-20 16:40:22 +0300

    initial commit

---
+++ SchoolSystem_Assignment21/src/main/java/com/example/schoolsystem_assignment21/ControllerAdvice/ControllerAdvice.java
@@ -0,0 +1,9 @@
+package com.example.schoolsystem_assignment21.ControllerAdvice;
+
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+    
+}
